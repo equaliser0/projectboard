0f57f2a1e90eab8dcd511f50621735aba90e3520
equaliser0 <accounts@equaliser.minimalcity.de>
2020-05-08 23:15:21 +0200

    Add Header

---
+++ src/components/Header/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="bg-gray-900">
+    <section class="container mx-auto">
+      <div class="flex w-full p-6">
+        <div class="w-1/3 text-6xl">
+          ❉
+        </div>
+        <div class="flex w-2/3 items-center">
+          Hi, there 👋
+        </div>
+      </div>
+      <div class="w-full p-6">
+        <h1 class="text-xl font-semibold">Awesome Ecosystem</h1>
+        <p>
+          We combine the best technologies of today to build the best ecosystem of tomorrow.
+        </p>
+      </div>
+    </section>
+  </section>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
