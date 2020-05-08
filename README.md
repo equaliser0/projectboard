d0a926af7a7644cf0e5c6883fdf4ec44a53f27e2
equaliser0 <accounts@equaliser.minimalcity.de>
2020-05-08 18:01:41 +0200

    Initialize VueJS and Tailwind

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# projectboard
-This will become the project board for fractools
+# Projectboard
+
+[frac.tools](https://github.com/fractools) Projectboard with State and Milestones.
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app" class="bg-gray-800">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <div class="">
+      <Milestones msg="Welcome to Your Vue.js App"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Milestones from './components/Milestones'
+
+export default {
+  name: 'App',
+  components: {
+    Milestones
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
