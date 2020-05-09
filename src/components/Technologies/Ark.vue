8d3d4a2c83170fabd7bbb6afdc9db7c374e258fc
equaliser0 <accounts@equaliser.minimalcity.de>
2020-05-09 22:56:32 +0200

    Update Technologies Components
    - Implement `marked`
    - SVGs clickable for descriptions
    - Update Ark Image to SVG

--- src/components/Technologies/Ark.vue
+++ src/components/Technologies/Ark.vue
@@ -1,3 +1,16 @@
 <template>
-  <img class="w-12" src="https://ark.io/storage/66/square.svg" alt="Ark">
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 1200 1200">
+    <rect id="Прямоугольник_скругл._углы_1" data-name="Прямоугольник, скругл. углы 1" style="fill: #c9292c;" class="cls-1" width="1200" height="1200" rx="90" ry="90"/>
+    <path id="Фигура_3_копия" data-name="Фигура 3 копия" class="cls-2" d="M1065.55,974.5l-464.3-491.13L133.553,974.5l467.7-754.97ZM518.582,683.248h164.2L601.25,598.727ZM810.751,816.88l-74.74-78.809H465.357l-74.74,78.809H810.751Z"/>
+  </svg>
 </template>
+
+<style>
+.cls-1 {
+
+}
+.cls-2 {
+  fill: #fff;
+  fill-rule: evenodd;
+}
+</style>
--- src/components/Technologies/index.vue
+++ src/components/Technologies/index.vue
@@ -1,14 +1,35 @@
 <template>
   <section class="container mx-auto p-6">
     <div class="">
-      <h1 class="text-xl font-semibold">Awesome Technologies</h1>
+      <h1>Awesome Technologies</h1>
     </div>
-    <div class="flex justify-between p-4">
-      <Vue class="text-6xl"/>
-      <Nuxt class="text-6xl"/>
-      <Nest class="text-6xl"/>
-      <Socket class="text-6xl"/>
-      <Ark class="text-6xl"/>
+    <div class="flex justify-between py-4">
+      <Ark
+        class="text-6xl"
+        @click.native="openTech('Arkecosystem')"/>
+      <Nest
+        class="text-6xl"
+        @click.native="openTech('NestJS')"/>
+      <Nuxt
+        class="text-6xl"
+        @click.native="openTech('NuxtJS')"/>
+      <Socket
+        class="text-6xl"
+        @click.native="openTech('SocketIO')"/>
+      <Tailwind
+        class="text-6xl"
+        @click.native="openTech('TailwindCSS')"/>
+      <Vue
+        class="text-6xl"
+        @click.native="openTech('VueJS')"/>
+    </div>
+    <div
+      class="bg-gray-700 rounded"
+      v-if="tech">
+      <div class="p-6">
+        <div class="" v-html="renderMarkdown()">
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -18,11 +39,21 @@ import Nuxt from './Nuxt'
 import Nest from './Nest'
 import Socket from './Socket'
 import Ark from './Ark'
+import Tailwind from './Tailwind'
 
 export default {
   name: 'Technologies',
   data() {
     return {
+      tech: null
+    }
+  },
+  methods: {
+    openTech(tech) {
+      this.tech === tech ? this.tech = null : this.tech = tech;
+    },
+    renderMarkdown() {
+      return this.$marked(`## Awesome ${this.tech}`)
     }
   },
   components: {
@@ -30,7 +61,8 @@ export default {
     Nuxt,
     Nest,
     Socket,
-    Ark
+    Ark,
+    Tailwind
   }
 }
 </script>
